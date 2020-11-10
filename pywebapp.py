cf493300bf9992fa9bfbfa15da91e6be7653b716
junbeomh <42974687+junbeomh@users.noreply.github.com>
2020-11-10 11:26:36 -0800

    Update pywebapp.py

--- pywebapp.py
+++ pywebapp.py
@@ -14,7 +14,7 @@ class Default:
         return """<html>
         <title>PyWebApp</title>
         <body bgcolor="gray">
-        <h1>Hello COMP4964!</h1>
+        <h1>John Han A01064824 </h1>
         <p>Available API methods:</p>
         <ul>
         <li><a href="/api/v1/test">/api/v1/test</a></li>
